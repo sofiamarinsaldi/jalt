80cf9049e289dd43a91f67eea001c0cebeb453da
Emanuele <egalli64@gmail.com>
2023-02-16 17:52:51 +0100

    first commit

---
+++ src/main/java/com/example/jtdd/s04/Simple.java
@@ -0,0 +1,23 @@
+/*
+ * Introduction to Test Driven Development with Java
+ * 
+ * https://github.com/egalli64/jtdd
+ */
+package com.example.jtdd.s04;
+
+/**
+ * A stub for a class that has to be developed
+ * 
+ * @see SimpleTest, its JUnit test case
+ */
+public class Simple {
+    /**
+     * Method stub, assume we have still to think about how it should work
+     * 
+     * @param value the input
+     * @return currently, always zero
+     */
+    public int negate(int value) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/example/jtdd/s04/SimpleTest.java
@@ -0,0 +1,27 @@
+/*
+ * Introduction to Test Driven Development with Java
+ * 
+ * https://github.com/egalli64/jtdd
+ */
+package com.example.jtdd.s04;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * A test case stub
+ * 
+ * @see Simple, the class to be tested
+ */
+class SimpleTest {
+    /**
+     * A test stub. Notice the Test annotation, without it, it is not a JUnit test.
+     *
+     * The call to Assertions::fail() causes a red light.
+     */
+    @Test
+    void testNegate() {
+        fail("Not yet implemented");
+    }
+}
