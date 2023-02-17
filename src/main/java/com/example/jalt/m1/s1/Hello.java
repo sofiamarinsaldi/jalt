7eb893562e8a63e0ffd24a0f5bb14f41891fdb2d
Emanuele <egalli64@gmail.com>
2023-02-17 12:38:31 +0100

    System.err vs System.out

---
+++ src/main/java/com/example/jalt/m1/s1/Hello.java
@@ -0,0 +1,30 @@
+/*
+ * Introduction to Java Logging and Testing
+ * 
+ * https://github.com/egalli64/jalt
+ */
+package com.example.jalt.m1.s1;
+
+import java.util.Arrays;
+
+/**
+ * A class using
+ * <li>System.out to generate user output
+ * <li>System.err to generate logging
+ */
+public class Hello {
+    public static void main(String[] args) {
+        // This message is meant to be seen by developers
+        System.err.println("User input by args: " + Arrays.toString(args));
+
+        // These messages are meant to be seen by users
+        if (args.length == 0) {
+            System.out.println("Please, pass me your name");
+        } else {
+            System.out.println("Hello, " + args[0]);
+        }
+
+        // This message is meant to be seen by developers
+        System.err.println("Done");
+    }
+}
