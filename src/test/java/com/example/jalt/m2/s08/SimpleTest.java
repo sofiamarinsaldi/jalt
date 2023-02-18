fadd2f45e48dc62e857383a2d85edf6f54f2cbf0
Emanuele <egalli64@gmail.com>
2023-02-18 19:38:31 +0100

    ParameterizedTest

---
+++ src/test/java/com/example/jalt/m2/s08/SimpleTest.java
@@ -0,0 +1,79 @@
+/*
+ * Introduction to Java Logging and Testing
+ * 
+ * https://github.com/egalli64/jalt
+ */
+package com.example.jalt.m2.s08;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import com.example.jalt.m2.s06.Simple;
+
+/**
+ * Jupiter parameterized tests
+ */
+class SimpleTest {
+    /** The instance to be tested is initialized in the same way for each test */
+    private Simple simple = new Simple();
+
+    /**
+     * Similar tests grouped together.
+     * 
+     * The parameter setting and conversion to the right type is done by JUnit
+     * 
+     * @param input    to be passed to the tested method
+     * @param expected the expected result
+     */
+    @ParameterizedTest
+    @CsvSource({ "12,true", "-42,false", "0,false" })
+    void isPositive(int input, boolean expected) {
+        boolean actual = simple.isPositive(input);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Using textBlock in CsvSource could improve readability
+     */
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+             42, -42
+            -42,  42
+              0,   0
+            """)
+    void checkEqual(int input, int expected) {
+        int actual = simple.negate(input);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * A missing CsvSource value is interpreted as null
+     * 
+     * A string could be delimited by _single_ quotes
+     */
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+              42,
+             -42, 'not a null'
+               0,  not a null
+            """)
+    void nullIfPositive(int input, String expected) {
+        String actual = simple.nullIfPositive(input);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Use as many parameters as you need
+     */
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            0.29, 3.0, 0.87, 0.000_000_000_000_000_2
+            2   , 3  , 6   , 0
+            """)
+    void checkEqualDouble(double first, double second, double expected, double delta) {
+        double actual = simple.multiply(first, second);
+        assertEquals(expected, actual, delta);
+    }
+}
