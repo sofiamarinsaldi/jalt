3151a3af2f6e6279cccc5de740fe8c02366d06e6
Emanuele <egalli64@gmail.com>
2023-02-24 11:40:26 +0100

    matchers

---
+++ src/main/java/com/example/jalt/m3/s05/MatchersExample.java
@@ -0,0 +1,29 @@
+/*
+ * Introduction to Java Logging and Testing
+ * 
+ * https://github.com/egalli64/jalt
+ */
+package com.example.jalt.m3.s05;
+
+/**
+ * Just for creating a mock that uses matchers
+ */
+public interface MatchersExample {
+    /**
+     * Usually return a "good" value, sometimes throw an exception
+     * 
+     * @param x primitive
+     * @return a result
+     * @throws IllegalArgumentException when the passed parameter is not accepted
+     */
+    int methodA(int x);
+
+    /**
+     * Usually return a "good" value, sometimes throw an exception
+     * 
+     * @param x a String
+     * @return a result
+     * @throws IllegalArgumentException when the passed parameter is not accepted
+     */
+    int methodB(String x);
+}
---
+++ src/test/java/com/example/jalt/m3/s05/AnInterfaceStubbedMockTest.java
@@ -0,0 +1,94 @@
+/*
+ * Introduction to Java Logging and Testing
+ * 
+ * https://github.com/egalli64/jalt
+ */
+package com.example.jalt.m3.s05;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.jalt.m3.s03.AnInterface;
+
+/**
+ * Using Mockito matchers
+ */
+class AnInterfaceStubbedMockTest {
+    private static final int GOOD_INT = 12;
+    private static final int BAD_INT = 21;
+    private static final String GOOD_STRING = "OK";
+    private static final String BAD_STRING = "KO";
+    private static final int MOCK_INT_RESULT = 42;
+    private static final String MOCK_STRING_RESULT = "OK";
+    private static final Throwable MOCK_EXCEPTION = new IllegalArgumentException();
+
+    private AnInterface mockOp = mock();
+
+    /**
+     * Any "good" int - Mockito::anyInt() - passed to calculate() should return a "good" value
+     * 
+     * It is not on the mock deciding which value is good
+     * 
+     * Stubbing by when .. thenReturn 
+     */
+    @Test
+    void calculateWhenThenReturn() {
+        // stubbing
+        when(mockOp.calculate(anyInt())).thenReturn(MOCK_INT_RESULT);
+
+        // usual testing, expecting GOOD_INT being an acceptable value
+        int input = GOOD_INT;
+        int actual = mockOp.calculate(input);
+        int expected = MOCK_INT_RESULT;
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    /**
+     * Any "good" int - Mockito::anyInt() - passed to calculate() should return a "good" value
+     * 
+     * Stubbing by doReturn ... when
+     */
+    @Test
+    void calculateDoReturnWhen() {
+        // stubbing
+        doReturn(MOCK_INT_RESULT).when(mockOp).calculate(anyInt());
+
+        // usual test (compressed), expecting GOOD_INT being an acceptable value
+        assertThat(mockOp.calculate(GOOD_INT)).isEqualTo(MOCK_INT_RESULT);
+    }
+
+    /**
+     * Any "bad" int passed to calculate() should cause an IllegalArgumentException
+     */
+    @Test
+    void calculateExceptional() {
+        when(mockOp.calculate(anyInt())).thenThrow(MOCK_EXCEPTION);
+        assertThatIllegalArgumentException().isThrownBy(() -> mockOp.calculate(BAD_INT));
+    }
+
+    /**
+     * Any "good" reference - Mockito::any() - passed to getMessage() should return a "good" value
+     */
+    @Test
+    void getMessage() {
+        when(mockOp.getMessage(any())).thenReturn(MOCK_STRING_RESULT);
+        assertThat(mockOp.getMessage(GOOD_STRING)).isEqualTo(MOCK_STRING_RESULT);
+    }
+
+    /**
+     * Any "bad" reference passed to getMessage() should cause an IllegalArgumentException
+     */
+    @Test
+    void getMessageExceptional() {
+        when(mockOp.getMessage(any())).thenThrow(MOCK_EXCEPTION);
+        assertThatIllegalArgumentException().isThrownBy(() -> mockOp.getMessage(BAD_STRING));
+    }
+}
