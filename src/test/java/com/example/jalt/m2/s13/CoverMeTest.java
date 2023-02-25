345b1f44a3feebb572f8f4989ded48ba8f5f984a
Emanuele <egalli64@gmail.com>
2023-02-25 23:07:02 +0100

    code coverage

---
+++ src/test/java/com/example/jalt/m2/s13/CoverMeTest.java
@@ -0,0 +1,47 @@
+/*
+ * Introduction to Java Logging and Testing
+ * 
+ * https://github.com/egalli64/jalt
+ */
+package com.example.jalt.m2.s13;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit Test to be used for practicing with code coverage
+ */
+class CoverMeTest {
+    private CoverMe cm = new CoverMe();
+
+    @Test
+    void deltaLeftBigger() {
+        assertThat(cm.delta(1, 0)).isEqualTo(1);
+    }
+
+    @Test
+    void deltaRightBigger() {
+        assertThat(cm.delta(0, 1)).isEqualTo(1);
+    }
+
+    @Test
+    void deltaSame() {
+        assertThat(cm.delta(0, 0)).isZero();
+    }
+
+    @Test
+    void arePositiveTF() {
+        assertThat(cm.arePositive(1, -1)).isFalse();
+    }
+
+    @Test
+    void arePositiveFT() {
+        assertThat(cm.arePositive(-1, 1)).isFalse();
+    }
+
+    @Test
+    void arePositiveTT() {
+        assertThat(cm.arePositive(1, 1)).isTrue();
+    }
+}
